8c12467e9b8b9928bc5bda5e6a806aae1845e734
Oscar Russo <odrusso1@gmail.com>
2021-04-19 20:16:06 +1200

    Tabulate grid traveler

--- python/grid_traveler.py
+++ python/grid_traveler.py
@@ -22,6 +22,26 @@ def grid_traveler_manual(n, m, memo={}):
     return memo[(n, m)]
 
 
+# I feel like I didn't -really- understand this properly.
+def grid_traveler_tab(x, y):
+    tab = [[0 for _ in range(x + 1)] for _ in range(y + 1)]
+    tab[1][1] = 1
+    for i in range(y + 1):
+        for j in range(x + 1):
+            old = tab[i][j]
+            if i + 1 <= y:
+                tab[i + 1][j] += old
+            if j + 1 <= x:
+                tab[i][j + 1] += old
+            tab[i][j] = old
+
+    # [print(row) for row in tab]
+    return tab[y][x]
+
 if __name__ == "__main__":
-    print(grid_traveler_easy(25, 25))  # should be 6
-    print(grid_traveler_manual(25, 25))  # should be 6
+    # print(grid_traveler_easy(25, 25))  # should be 6
+    # print(grid_traveler_manual(25, 25))  # should be 6
+
+    print(grid_traveler_tab(3, 3))  # should be 6
+    print(grid_traveler_tab(4, 7))  # should be >6
+    print(grid_traveler_tab(18, 18))  # should be >>>6
